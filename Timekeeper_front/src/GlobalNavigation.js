f40bf3950b5317493b826e4ab7225adb4815d046
thisisvv <58626061+thisisvv@users.noreply.github.com>
2022-09-22 21:46:31 +0200

    Erste verbindung zwischen front+backend
    Als basis die Sidequest aus dem unterricht.
    Tabelle erstellt und einträge aus dem neuen Timekeeper backend abgefragt.

---
+++ Timekeeper_front/src/GlobalNavigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class GlobalNavigation extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/quiz">Quiz</Link>
+              </li>
+              <li>
+                <Link to="/tablecontainer">TableContainer</Link>
+              </li>
+              <li>
+                <Link to="/aboutus">About Us</Link>
+              </li>
+            </ul>
+          </nav>
+        )
+    }
+}
+
+export default GlobalNavigation;
